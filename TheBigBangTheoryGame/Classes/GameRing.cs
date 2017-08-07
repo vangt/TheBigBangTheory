92282af4bb7ecd28c0029d418ce69e5ec2b2651b
vangt <vangt86@gmail.com>
2017-08-07 09:04:35 -0500

    Created GetChoices method to return string of choice.

--- TheBigBangTheoryGame/Classes/GameRing.cs
+++ TheBigBangTheoryGame/Classes/GameRing.cs
@@ -81,5 +81,36 @@ namespace TheBigBangTheoryGame.Classes
                 GetOutCome(player1, player2);
             }
         }
+
+        public string GetChoices(int number)
+        {
+            Console.WriteLine("Player please choose your selection.");
+            Console.WriteLine("Choose a number \n 1: Rock \n 2: Paper \n 3: Scissor \n 4: Lizard \n 5: Spock \n");
+
+            string choice = GetNumber(number);
+
+            switch (choice)
+            {
+                case "1":
+                    choice = "Rock";
+                    return choice;
+                case "2":
+                    choice = "Paper";
+                    return choice;
+                case "3":
+                    choice = "Scissor";
+                    return choice;
+                case "4":
+                    choice = "Lizard";
+                    return choice;
+                case "5":
+                    choice = "Spock";
+                    return choice;
+                default:
+                    Console.WriteLine("You did not choose a number on the list.");
+                    choice = GetChoices(number);
+                    return choice;
+            }
+        }
     }
 }
