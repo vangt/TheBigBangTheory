d7a00e48da5e3bc7cfc8a761bb1950b52b569658
vangt <vangt86@gmail.com>
2017-08-07 08:14:44 -0500

    Created GameRing class and added member variable list of players.

---
+++ TheBigBangTheoryGame/Classes/GameRing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBigBangTheoryGame.Classes
+{
+    public class GameRing
+    {
+        List<Players> players = new List<Players>();
+    }
+}
